03cd8ec9387d53abb13f4759e77f2baa7a1ad61e
koszav <zavos@tuta.io>
2023-02-13 22:31:13 +0200

    Add event listeners on the nav bar buttons

--- src/index.js
+++ src/index.js
@@ -1,3 +1,17 @@
 import mainPage from "./mainPage";
+import menu from "./menu";
 
 mainPage();
+
+const homeBtn = document.querySelector("#home");
+homeBtn.addEventListener("click", () => {
+  alert("Hello World");
+});
+
+const menuBtn = document.querySelector("#menu");
+menuBtn.addEventListener("click", () => {
+  alert("Hello second");
+});
+
+// const menuButton = document.getElementById(menu);
+// menuButton.onclick = menu();
---
+++ src/menu.js
@@ -0,0 +1,5 @@
+export default function menu() {
+  const content = document.querySelector("#content");
+  content.textContent = "works?";
+  alert("works?");
+}
