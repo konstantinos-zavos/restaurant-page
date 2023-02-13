4aade944b29017c52bd0a277faa9b2717c99b806
koszav <zavos@tuta.io>
2023-02-13 22:52:29 +0200

    Add dummy content on contact tab(module)

--- src/contact.js
+++ src/contact.js
@@ -5,4 +5,9 @@ export default function contact() {
   const p1 = document.createElement("p");
   p1.textContent = "Telephone number: 555 - 555 - 123";
   content.appendChild(p1);
+
+  const p2 = document.createElement("p");
+  p2.textContent =
+    "This is my WebPack training project! Note to self: Make it fun for you or semi-enjoyable!";
+  content.appendChild(p2);
 }
